cff2df207fe21c817305bd4a129975af198020f3
Mariano Moreno <mariano25294@gmail.com>
2019-09-28 18:27:48 +0200

    [first commit]

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <windows.h>
 
 int main() {
-    int count = 10000;
+    int count = 1000000;
     do {
         SetCursorPos(200, 200);
         SetCursorPos(300, 300);
         count--;
-    } while (count > 1);
+    } while (count >= 1);
 }
